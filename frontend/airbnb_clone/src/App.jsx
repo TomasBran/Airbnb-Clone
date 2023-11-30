51d927224bf57dc9cc35d0283ef7b583976c003f
Tomas Bran <tomasaugusto.bran@hotmail.com>
2023-11-30 14:36:39 -0300

    register and login form

--- frontend/airbnb_clone/src/App.jsx
+++ frontend/airbnb_clone/src/App.jsx
@@ -1,14 +1,46 @@
-import { Button } from '@material-tailwind/react'
-import './App.css'
+  import { useEffect, useState } from "react";
+  import {
+    Button,
+    Dialog,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Input,
+    Checkbox,
+  } from '@material-tailwind/react'
+  import './App.css'
+  import Forms from "./components/forms/Forms";
 
-function App() {
-  
 
-  return (
-    <>
+
+  function App() {
+
     
-    </>
-  )
-}
+    const [openLogin, setOpenLogin] = useState(false);
+    const handleOpenLogin = () => {
+      setOpenLogin((cur) => !cur)
+    };
 
-export default App
+    const [openSignUp, setOpenSignUp] = useState(false);
+    const handleOpenSignUp = () => setOpenSignUp((cur) => !cur);
+    
+
+
+    return (
+      <>
+        <Button onClick={handleOpenLogin}>Iniciar Sesión</Button>
+        <Button onClick={handleOpenSignUp}>Registrarse</Button>
+        <Forms
+          openLogin={openLogin}
+          openSignUp={openSignUp}
+          handleOpenLogin={handleOpenLogin}
+          handleOpenSignUp={handleOpenSignUp}
+        />
+        
+      </>
+    )
+  }
+
+  export default App
