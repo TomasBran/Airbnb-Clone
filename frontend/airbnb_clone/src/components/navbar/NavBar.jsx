f2fc2bd52be174cbbf1b83e4e37bb3dd7e1e465c
Tomas Bran <tomasaugusto.bran@hotmail.com>
2023-12-07 16:28:04 -0300

    agregado boton temporal de registro propiedad

--- frontend/airbnb_clone/src/components/navbar/NavBar.jsx
+++ frontend/airbnb_clone/src/components/navbar/NavBar.jsx
@@ -54,7 +54,13 @@ const NavBar = () => {
                         {isSearchOpen && <Search/>}
                     </div>
                     <div className="flex flex-row gap-px md:gap-4">
-                        
+
+                        {/* BOTÓN DE REGISTRO DE PROPIEDAD ES TEMPORAL */}
+                        <Link to="/property-register">
+                            <Button variant="text">Registro de Propiedad</Button>
+                        </Link>
+                        {/* BOTÓN DE REGISTRO DE PROPIEDAD ES TEMPORAL */}
+
                         <Button variant="text" className="rounded-full hidden sm:inline" onClick={handleOpenSignUp}>Registrarse</Button>
                         <Button variant="outlined" className="rounded-full" onClick={handleOpenLogin}>Iniciar Sesión</Button>
                         <Forms
