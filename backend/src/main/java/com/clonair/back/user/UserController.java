15c4ae2aee6b515500466f9a4ad55c32ddd158e5
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2023-12-15 14:22:12 -0300

    Se agrega logica de delete user en el controller y service

--- backend/src/main/java/com/clonair/back/user/UserController.java
+++ backend/src/main/java/com/clonair/back/user/UserController.java
@@ -1,22 +1,20 @@
 
 package com.clonair.back.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getOne(@PathVariable String id, @RequestHeader(name = HttpHeaders.AUTHORIZATION) String token) throws Exception {
@@ -26,6 +24,16 @@ public class UserController {
     @GetMapping()
     public ResponseEntity<List<UserResponse>> getAll(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String token) throws Exception {
         return ResponseEntity.ok(userService.getAll(token));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable String id, @RequestHeader(name = HttpHeaders.AUTHORIZATION) String token) {
+        try {
+            userService.delete(id, token);
+            return ResponseEntity.ok("User deleted successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
 }
